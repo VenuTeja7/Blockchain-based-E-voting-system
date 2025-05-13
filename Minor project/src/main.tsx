9cdd22ee662747065dd54a280dd622c29850aaeb
VenuTeja7 <vtu23343@veltech.edu.in>
2025-05-13 13:48:28 +0530

    Add files via upload

---
+++ Minor project/src/main.tsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.tsx';
+import './index.css';
+import '@fontsource/poppins/300.css';
+import '@fontsource/poppins/400.css';
+import '@fontsource/poppins/500.css';
+import '@fontsource/poppins/700.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+      <ToastContainer position="bottom-right" />
+    </BrowserRouter>
+  </StrictMode>
+);
